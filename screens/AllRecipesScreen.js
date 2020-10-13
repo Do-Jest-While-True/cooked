895a370080f866fbefbda844e87d0f4fef289aa6
Bryan <bryanryu1@gmail.com>
2020-10-13 17:55:39 -0400

    added pull to refresh feature on allrecipesscreen

--- screens/AllRecipesScreen.js
+++ screens/AllRecipesScreen.js
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react'
-import { FlatList, SafeAreaView } from 'react-native'
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+} from 'react-native'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
 import { connect } from 'react-redux'
 
@@ -9,33 +14,54 @@ import { getRecipes } from '../redux'
 
 import defaultStyles from '../config/defaultStyles'
 
+const wait = (timeout) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout)
+  })
+}
+
 const AllRecipesScreen = ({ navigation, getRecipes, recipes }) => {
+  const [refreshing, setRefreshing] = React.useState(false)
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true)
+    wait(1000).then(() => setRefreshing(false))
+    getRecipes()
+  }, [])
+
   useEffect(() => {
     getRecipes()
   }, [])
+
   return (
     <SafeAreaView style={defaultStyles.container}>
-      {recipes && (
-        <FlatList
-          data={recipes}
-          keyExtractor={(recipe) => recipe.id.toString()}
-          renderItem={({ item }) => (
-            <Swipeable
-              renderRightActions={() => <SwipeDeleteBtn id={item.id} />}
-              onSwipeableRightOpen={() => console.log('delete opened')}
-            >
-              <RecipeListItem
-                name={item.name}
-                // refactor img:
-                imageUrl={tempImgUrl}
-                time={item.time}
-                id={item.id}
-                nav={navigation}
-              />
-            </Swipeable>
-          )}
-        />
-      )}
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {recipes && (
+          <FlatList
+            data={recipes}
+            keyExtractor={(recipe) => recipe.id.toString()}
+            renderItem={({ item }) => (
+              <Swipeable
+                renderRightActions={() => <SwipeDeleteBtn id={item.id} />}
+                onSwipeableRightOpen={() => console.log('delete opened')}
+              >
+                <RecipeListItem
+                  name={item.name}
+                  // refactor img:
+                  imageUrl={tempImgUrl}
+                  time={item.time}
+                  id={item.id}
+                  nav={navigation}
+                />
+              </Swipeable>
+            )}
+          />
+        )}
+      </ScrollView>
     </SafeAreaView>
   )
 }
