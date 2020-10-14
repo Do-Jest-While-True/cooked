8c4d48f5d0484e6af11d5598442cf5662bb866c8
Xander Bakx <xanderbakx@gmail.com>
2020-10-14 13:51:19 -0400

    fix: update user store to include api routes

--- redux/user.js
+++ redux/user.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-// import { createBrowserHistory as history } from 'history'
 import { URL } from './serverUrl'
 
 /**
  * ACTION TYPES
  */
 const GET_USER = 'GET_USER'
+const GET_USERS = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
 
 /**
@@ -17,11 +17,33 @@ const defaultUser = {}
  * ACTION CREATORS
  */
 const getUser = (user) => ({ type: GET_USER, user })
+const getUsers = (users) => ({ type: GET_USERS, users })
 const removeUser = () => ({ type: REMOVE_USER })
 
 /**
  * THUNK CREATORS
  */
+
+// USER ROUTES
+export const gotUser = (userId) => async (dispatch) => {
+  try {
+    const { res: user } = await axios.get(`${URL}/api/users/${userId}`)
+    dispatch(getUser(user))
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const gotUsers = () => async (dispatch) => {
+  try {
+    const { res: users } = await axios.get(`${URL}/api/users`)
+    dispatch(getUsers(users))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// AUTH ROUTES
 export const me = () => async (dispatch) => {
   try {
     const res = await axios.get(`${URL}/auth/me`)
@@ -62,6 +84,8 @@ export default function (state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
       return action.user
+    case GET_USERS:
+      return action.users
     case REMOVE_USER:
       return defaultUser
     default:
