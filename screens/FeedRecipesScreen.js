c7e1377fbd6715e5a9ee1e23b20e2e1b9031ae6e
ryanpsmith26 <ryanpsmith01@gmail.com>
2020-10-15 12:34:36 -0400

    changes allrecipes to feedrecipescreen

--- screens/FeedRecipesScreen.js
+++ screens/FeedRecipesScreen.js
@@ -5,14 +5,13 @@ import {
   ScrollView,
   RefreshControl,
 } from 'react-native'
-// import Swipeable from 'react-native-gesture-handler/Swipeable'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 import { connect } from 'react-redux'
 
 import RecipeListItem from '../components/RecipeListItem'
 import SwipeDeleteBtn from '../components/SwipeDeleteBtn'
-import { getRecipes } from '../redux'
+import { getFeedRecipes } from '../redux'
 
-import colors from '../config/colors'
 import defaultStyles from '../config/defaultStyles'
 
 const wait = (timeout) => {
@@ -21,28 +20,23 @@ const wait = (timeout) => {
   })
 }
 
-const AllRecipesScreen = ({ navigation, getRecipes, recipes }) => {
+const FeedRecipesScreen = ({ navigation, getFeedRecipes, recipes }) => {
   const [refreshing, setRefreshing] = React.useState(false)
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true)
     wait(1000).then(() => setRefreshing(false))
-    getRecipes()
+    getFeedRecipes()
   }, [])
 
   useEffect(() => {
-    getRecipes()
+    getFeedRecipes()
   }, [])
-
   return (
     <SafeAreaView style={defaultStyles.container}>
       <ScrollView
         refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-            tintColor={colors.white}
-          />
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
       >
         {recipes && (
@@ -50,18 +44,19 @@ const AllRecipesScreen = ({ navigation, getRecipes, recipes }) => {
             data={recipes}
             keyExtractor={(recipe) => recipe.id.toString()}
             renderItem={({ item }) => (
-              // <Swipeable
-              // 	renderRightActions={() => <SwipeDeleteBtn id={item.id} />}
-              // 	onSwipeableRightOpen={() => console.log('delete opened')}
-              // >
-              <RecipeListItem
-                name={item.name}
-                imageUrl={item.imageUrl}
-                time={item.time}
-                id={item.id}
-                nav={navigation}
-              />
-              // </Swipeable>
+              <Swipeable
+                renderRightActions={() => <SwipeDeleteBtn id={item.id} />}
+                onSwipeableRightOpen={() => console.log('delete opened')}
+              >
+                <RecipeListItem
+                  name={item.name}
+                  imageUrl={item.imageUrl}
+                  time={item.time}
+                  recipeId={item.id}
+                  user={item.user}
+                  nav={navigation}
+                />
+              </Swipeable>
             )}
           />
         )}
@@ -71,11 +66,11 @@ const AllRecipesScreen = ({ navigation, getRecipes, recipes }) => {
 }
 
 const mapState = (state) => ({
-  recipes: state.recipes,
+  recipes: state.feedRecipes,
 })
 
 const mapDispatch = (dispatch) => ({
-  getRecipes: () => dispatch(getRecipes()),
+  getFeedRecipes: () => dispatch(getFeedRecipes()),
 })
 
-export default connect(mapState, mapDispatch)(AllRecipesScreen)
+export default connect(mapState, mapDispatch)(FeedRecipesScreen)
