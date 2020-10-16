cbd1b15ec3beaa853c8c74eb04bbfa686e274405
Xander Bakx <xanderbakx@gmail.com>
2020-10-16 14:35:28 -0400

    Update RecipeListItem.js

--- components/RecipeListItem.js
+++ components/RecipeListItem.js
@@ -41,7 +41,7 @@ const RecipeListItem = ({
     return (
       <TouchableOpacity
         style={styles.listItemView}
-        onPress={() => nav.navigate('Recipe', { recipeId })}
+        onPress={() => nav.navigate('Recipe', { recipeId, userId, nav })}
       >
         <Image source={{ uri: imageUrl }} style={styles.listItemImg} />
         <View>
